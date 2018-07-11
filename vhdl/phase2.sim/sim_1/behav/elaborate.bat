40b4d3cf341a819b0479554b6dc7722696fe10ce
fatisati <fatisati@gmail.com>
2018-07-11 11:09:53 +0430

    vhdl src only

--- vhdl/phase2.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2015.4\\bin
-call %xv_path%/xelab  -wto 82de61160d1144b591aa5512e9b7cf45 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L secureip --snapshot testbench_behav xil_defaultlib.testbench -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
