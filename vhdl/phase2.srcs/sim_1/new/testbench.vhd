853d8266f31e4537144ebc5be12de6126a0143a3
fatisati <fatisati@gmail.com>
2018-07-13 13:47:22 +0430

    testbench modified

--- vhdl/phase2.srcs/sim_1/new/testbench.vhd
+++ vhdl/phase2.srcs/sim_1/new/testbench.vhd
@@ -42,13 +42,13 @@ architecture Behavioral of testbench is
 component sha is
   generic (length : integer := 31);
   Port ( msg: in unsigned(length downto 0);
-          clk,rst,en1,en2: in std_logic;
+          clk,rst,en: in std_logic;
           ready: out std_logic;
           final_hash: out unsigned(255 downto 0)
   );
 end component;
 component mining is
-  Port (clk,rst,en1,en2: in std_logic;
+  Port (clk,rst,en: in std_logic;
         block_header: in unsigned (639 downto 0);
         hash: out unsigned (255 downto 0);
         finish: out std_logic
@@ -64,15 +64,14 @@ signal finish: std_logic;
 
 begin
 
-rst <= '1', '0' after 30 ns, '1' after 130 ns, '0' after 160 ns, '1' after 360 ns, '0' after 390 ns;
+rst <= '1', '0' after 30 ns;--, '1' after 130 ns, '0' after 160 ns, '1' after 360 ns, '0' after 390 ns;
 clk <= not(clk) after 50 ns;
-en1<='0', '1' after 260 ns, '0' after 360 ns;
-en2<='0', '1' after 360 ns, '0' after 550 ns;
+en1<='1', '0' after 500 ns;
 
 msg <= "01100001011000100110001101100100"; --abcd--"00000000011000010110001001100011"; --abc
 
-u: sha generic map(31) port map (msg,clk,rst,en1,en2,ready,final_hash);
+u: sha generic map(31) port map (msg,clk,rst,en1,ready,final_hash);
 block_header <= x"02000000" & x"17975b97c18ed1f7e255adf297599b55330edab87803c8170100000000000000" 
 & final_hash & x"358b0553" & x"5350f119" & x"00000000";
-l: mining  port map ( clk,rst,en1,en2, block_header, hash, finish);
+l: mining  port map ( clk,rst,ready, block_header, hash, finish);
 end Behavioral;
